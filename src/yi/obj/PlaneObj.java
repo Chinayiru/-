026d066915ee55b4d28d239bfcbcc0ba8f342fe7
yiru <yi_ru@qq.com>
2022-11-24 16:28:17 +0800

    界面飞机

---
+++ src/yi/obj/PlaneObj.java
@@ -0,0 +1,9 @@
+package yi.obj;
+
+/**
+ * @aythor yi
+ * @data 2022/11/24 itime
+ * @Description
+ */
+public class PlanObj {
+}
---
+++ src/yi/until/GameUntil.java
@@ -0,0 +1,14 @@
+package yi.win.until;
+
+import sun.awt.image.ToolkitImage;
+
+import java.awt.*;
+
+/**
+ * @aythor yi
+ * @data 2022/11/24  14:42:43
+ * @Description
+ */
+public class GameUntil {
+    public static Image bgImg = Toolkit.getDefaultToolkit().getImage("static/imgs/bgmenu.jpg");
+}
